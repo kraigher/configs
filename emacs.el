9d03abdb832252a371bd4934f97e79a18e3123d6
Olof Kraigher <olof.kraigher@gmail.com>
2018-03-21 20:38:32 +0100

    Update emacs.el

--- emacs.el
+++ emacs.el
@@ -1,3 +1,8 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
 (setq inhibit-splash-screen t)
 (load-theme 'wombat)
 (ido-mode)
@@ -8,21 +13,43 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(setq-default buffer-file-coding-system 'utf-8-unix)
+
+(set-face-attribute 'default nil
+                    :family "Inconsolata" :height 120)
 
 ; Highlight trailing whitespace
 (require 'whitespace)
 (setq whitespace-style '(face empty tabs lines-tail trailing))
 (setq whitespace-line-column 119)
 (global-whitespace-mode t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ; To make escaped characters work
 (require 'iso-transl)
 
+; VHDL
 (setq vhdl-electric-mode 'nil);
 
+; Rust
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+(setq-default racer-rust-src-path "~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src")
 
-(setq-default buffer-file-coding-system 'utf-8-unix)
+(require 'rust-mode)
+(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
 
-(set-face-attribute 'default nil
-                    :family "Inconsolata" :height 120)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (magit company racer))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
